37f424d2ab5c8f27f10e947f278d8b5e372875fd
khajean202020 <khajean202020@gmail.com>
2025-01-14 22:45:28 +0000

    push all files

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -16,5 +16,6 @@ driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
+        driver.quit();
     }
 }
